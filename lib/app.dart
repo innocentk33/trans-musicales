9ada4442c8c21b292720995fd5d34e33b5620b48
innocent <innocent.kacou@niji.fr>
2025-02-11 22:41:48 +0100

    WIP : register

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:trans_musicales/core/theme/app_palette.dart';
-import 'package:trans_musicales/presentation/home.dart';
+import 'package:trans_musicales/presentation/auth/register_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: AppPalette.primary),
         useMaterial3: true,
       ),
-      home: const Home(),
+      home: RegisterPage(),
     );
   }
 }
